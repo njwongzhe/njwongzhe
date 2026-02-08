01ef0eafc86991385401f08b68f02edb6e7f18b2
njwongzhe <ngjaawei@gmail.com>
2026-02-09 04:05:50 +0800

    push

--- Flutter/Dart/20 - Generics (Collection).dart
+++ Flutter/Dart/20 - Generics (Collection).dart
@@ -3,9 +3,9 @@ void main(List<String> args) {
   // This helps catch type-related errors at compile time and improves code readability.
   // "<Type>" syntax is used to define the type of elements in collections like "List" and "Map".
 
-  // Create a "List" that can only can hold integers
-  List<int> intList = [1, 2, 3, 4, 5];
+  List<int> intList = [1, 2, 3, 4, 5]; // Create a "List" that can only can hold integers
+  print("intList  = $intList");        // Output: [1, 2, 3, 4, 5]
   
-  // Create a "Map" that maps strings to integers
-  Map<String, int> scoreMap = {"Alice": 90, "Bob": 85, "Charlie": 95};
+  Map<String, int> scoreMap = {"Alice": 90, "Bob": 85, "Charlie": 95}; // Create a "Map" that maps strings to integers
+  print("scoreMap = $scoreMap");                                       // Output: {Alice: 90, Bob: 85, Charlie: 95}
 }
