a61f98cc9e0c0deb3a151d9a6e54bef52354dec6
njwongzhe <ngjaawei@gmail.com>
2026-05-13 10:37:34 +0800

    push

--- 01 - Web Programming/06 - PHP/13 - PHP Connect Database - Insert.php
+++ 01 - Web Programming/06 - PHP/13 - PHP Connect Database - Insert.php
@@ -6,11 +6,16 @@
 
     if ($name != "" && $calories != "") {
         // Insert a new record into the "fruits" table.
-        $sql = "INSERT INTO fruits (name, calories) VALUES ('$name', '$calories')";
+        // However, the following code is vulnerable to SQL injection attacks. Do NOT use it in production code.
+        // $sql = "INSERT INTO fruits (name, calories) VALUES ('$name', '$calories')";
+
+        // Secure the code by using prepared statements to prevent SQL injection attacks.
+        $stmt = $db_connect->prepare("INSERT INTO fruits (name, calories) VALUES (?, ?)");
+        $stmt->bind_param("ss", $name, $calories); // "ss" means both parameters are strings. ($name and $calories are both strings.)
+        // "ss" can be replaced to other combinations of "i" (integer), "d" (double), "s" (string) and "b" (binary) depending on the data types of the parameters.
 
         // Check if the insertion was successful.
-        if ($db_connect->query($sql) === TRUE) {
-            echo "<pre>$sql</pre>\n";
+        if ($stmt->execute() === TRUE) {
             echo "Successful add new fruit.";
             echo "<p><a href='13 - PHP Connect Database - Main.php'>Back to Main Page</a></p>";
             echo "<p><a href='13 - PHP Connect Database - Insert.html'>Add Another Fruit</a></p>";
