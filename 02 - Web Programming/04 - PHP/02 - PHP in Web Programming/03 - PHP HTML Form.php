2fb49a9f6da648fabe85ed5330b9a607f0119572
njwongzhe <ngjaawei@gmail.com>
2026-07-05 03:30:34 +0800

    PHP Updated

--- 02 - Web Programming/04 - PHP/02 - PHP in Web Programming/03 - PHP HTML Form.php
+++ 02 - Web Programming/04 - PHP/02 - PHP in Web Programming/03 - PHP HTML Form.php
@@ -1,3 +1,5 @@
 <?php
+    // Read numbers from POST request and display the sum.
+    // The "num1" and "num2" are the "name" attributes of the input tags in the HTML form.
     echo $_POST["num1"] + $_POST["num2"];
 ?>
